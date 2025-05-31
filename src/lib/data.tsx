f88772a38d944d77fdbc9626fa10830029a1cf91
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2025-05-31 17:41:35 +0330

    remove testimonials section

--- src/lib/data.tsx
+++ src/lib/data.tsx
@@ -62,10 +62,10 @@ export const NAV_LINKS = [
     label: "Work",
     href: "#work",
   },
-  {
-    label: "Testimonials",
-    href: "#testimonials",
-  },
+  // {
+  //   label: "Testimonials",
+  //   href: "#testimonials",
+  // },
   {
     label: "Contact",
     href: "#contact",
