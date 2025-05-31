f94a135c991198e37c1d5c24671dbd8e4d9b1a77
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2025-05-31 16:12:40 +0330

    edit about me section

--- src/components/sections/about-me.tsx
+++ src/components/sections/about-me.tsx
@@ -1,11 +1,9 @@
-import Image from 'next/image';
+import Image from "next/image";
 
-import SagarFullPose from '/public/images/sagar-full-pose.jpg';
-import Tag from '@/components/data-display/tag';
-import Container from '@/components/layout/container';
-import Typography from '@/components/general/typography';
-import Link from '@/components/navigation/link';
-import { EXTERNAL_LINKS } from '@/lib/data';
+import mostafaFullPose from "/public/images/mostafa-full-pose.jpg";
+import Tag from "@/components/data-display/tag";
+import Container from "@/components/layout/container";
+import Typography from "@/components/general/typography";
 
 const AboutMeSection = () => {
   return (
@@ -19,12 +17,12 @@ const AboutMeSection = () => {
         <div className="flex justify-center md:order-first md:justify-end">
           <div className="relative h-[380px] w-[320px] md:h-[460px] md:w-[380px] lg:h-[520px] lg:w-[440px]">
             <Image
-              src={SagarFullPose}
-              alt="Fullpose of Sagar"
-              className="absolute z-10 h-[360px] w-[280px] border-8 border-gray-50 max-md:left-5 md:right-0 md:top-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"
-              style={{ objectFit: 'cover' }}
+              src={mostafaFullPose}
+              alt="Fullpose of mostafa"
+              className="absolute z-10 h-[360px] w-[280px] rounded-3xl border-8 border-gray-50 max-md:left-5 md:right-0 md:top-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"
+              style={{ objectFit: "cover" }}
             ></Image>
-            <div className="absolute h-[360px] w-[320px] border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"></div>
+            <div className="absolute h-[360px] w-[320px] rounded-xl border-8 border-transparent bg-gray-200 max-md:top-5 md:bottom-0 md:left-0 md:h-[420px] md:w-[340px] lg:h-[480px] lg:w-[400px]"></div>
           </div>
         </div>
 
@@ -34,74 +32,27 @@ const AboutMeSection = () => {
             Curious about me? Here you have it:
           </Typography>
           <Typography>
-            I&apos;m a passionate,{' '}
-            <Link
-              noCustomization
-              externalLink
-              withUnderline
-              href={EXTERNAL_LINKS.FIGMA}
-            >
-              self-proclaimed designer
-            </Link>{' '}
-            who specializes in full stack development (React.js & Node.js). I am
-            enthusiastic about bringing the technical and visual aspects of
-            digital products to life. User experience, pixel perfect design, and
-            writing clear, readable, highly performant code matters to me.
+            I&apos;m a passionate, a front-end developer with a strong passion
+            for crafting clean, responsive, and visually appealing user
+            interfaces. My journey in programming began in 2021, and since then,
+            I&apos;ve been deeply fascinated by the web — especially the
+            creative side of front-end development. I enjoy turning designs into
+            interactive digital experiences that not only look good but also
+            perform well across all devices.
           </Typography>
           <Typography>
-            I began my journey as a web developer in 2015, and since then,
-            I&apos;ve continued to grow and evolve as a developer, taking on new
-            challenges and learning the latest technologies along the way. Now,
-            in my early thirties, 7 years after starting my web development
-            journey, I&apos;m building cutting-edge web applications using
-            modern technologies such as Next.js, TypeScript, Nestjs,
-            Tailwindcss, Supabase and much more.
+            While my main expertise lies in front-end technologies like HTML,
+            CSS, JavaScript, React, and etc..., I also have hands-on experience
+            with back-end development using NestJS. This full-stack
+            understanding helps me collaborate better with other developers and
+            build more cohesive and efficient applications.
           </Typography>
           <Typography>
-            I am very much a progressive thinker and enjoy working on products
-            end to end, from ideation all the way to development.
-          </Typography>
-          <Typography>
-            When I&apos;m not in full-on developer mode, you can find me
-            hovering around on twitter or on indie hacker, witnessing the
-            journey of early startups or enjoying some free time. You can follow
-            me on{' '}
-            <Link
-              noCustomization
-              externalLink
-              withUnderline
-              href={EXTERNAL_LINKS.TWITTER}
-            >
-              Twitter
-            </Link>{' '}
-            where I share tech-related bites and build in public, or you can
-            follow me on{' '}
-            <Link
-              noCustomization
-              externalLink
-              withUnderline
-              href={EXTERNAL_LINKS.GITHUB}
-            >
-              GitHub
-            </Link>
-            .
-          </Typography>
-          <Typography>Finally, some quick bits about me.</Typography>
-          <div className="flex flex-col gap-2 md:flex-row md:gap-6">
-            <ul className="flex list-inside list-disc flex-col gap-2">
-              <Typography component="li">
-                B.E. in Computer Engineering
-              </Typography>
-              <Typography component="li">Full time freelancer</Typography>
-            </ul>
-            <ul className="flex list-inside list-disc flex-col gap-2">
-              <Typography component="li">Avid learner</Typography>
-              <Typography component="li">Aspiring indie hacker</Typography>
-            </ul>
-          </div>
-          <Typography>
-            One last thing, I&apos;m available for freelance work, so feel free
-            to reach out and say hello! I promise I don&apos;t bite 😉
+            What truly excites me is seeing beautifully designed UI concepts —
+            they spark my creativity and motivate me to bring the same level of
+            detail and polish to the projects I build. I&apos;m always exploring
+            new tools and techniques to improve my work and stay aligned with
+            modern web trends.
           </Typography>
         </div>
       </div>
